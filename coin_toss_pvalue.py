a309e4a849ef8d2328f37ff6d0c3f3b38bf16a03
Debayan Mitra <52120078+debayanmitra1993-data@users.noreply.github.com>
2022-11-05 23:35:53 +0530

    Create coin_toss_pvalue.py

---
+++ coin_toss_pvalue.py
@@ -0,0 +1,18 @@
+import math
+
+def compute_pvalue(tosses):
+    n,x = 0,0
+    for idx in range(len(tosses)):
+        if tosses[idx] == "H":
+            x += 1
+        n += 1
+    
+    factorials = [-1]*n
+    for idx in range(n):
+        if idx == 0:
+            factorials[idx] = 1
+        else:
+            factorials[idx] = factorials[idx - 1]*(idx + 1)
+        
+    pvalue = (factorials[n - 1]*math.pow(0.5, x)*math.pow(0.5, n - x))/(factorials[x - 1]*factorials[n-x - 1])
+    return pvalue
